40d69c0e1b182574ea21e141002d02f1aa45ee98
Yash Gupta <yash.g@connectandheal.com>
2024-12-09 01:10:36 +0530

    add task: format_prices which uses DockerOperator -> update requirements.txt to install docker provider in airflow env, fix Dockerfile's SPARK_APPLICATION_ARG, this task works

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,6 +3,7 @@ from airflow.hooks.base import BaseHook
 from datetime import datetime
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 import requests
 
 from include.stock_market.tasks import _get_stock_prices, _store_prices
@@ -51,6 +52,22 @@ def stock_market(): # dag_id = stock_market
         }
     )
 
-    is_api_available() >> get_stock_prices >> store_prices
+    format_prices = DockerOperator(
+        task_id='format_prices',
+        image='airflow/stock-app',
+        container_name='format_prices',
+        api_version='auto',
+        auto_remove=True,
+        docker_url='tcp://docker-proxy:2375',
+        network_mode='container:spark-master',
+        tty=True,
+        xcom_all=False,
+        mount_tmp_dir=False,
+        environment={
+            'SPARK_APPLICATION_ARG': '{{ task_instance.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 stock_market()
