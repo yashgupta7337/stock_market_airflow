aa002fd24a5e9bd4e926f8667b8d4b591384fc33
Yash Gupta <yash.g@connectandheal.com>
2024-12-08 22:49:31 +0530

    add get_stock_prices tasks and its PythonOperator, fix folder name for include_copy

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,7 +2,12 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from datetime import datetime
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 import requests
+
+from include.stock_market.tasks import _get_stock_prices
+
+SYMBOL = 'META'
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -11,16 +16,25 @@ import requests
     tags=['stock_market', '1st_dag', '$META']
 )
 
-def stock_market(): # dag id = stock_market
+def stock_market(): # dag_id = stock_market
     
     @task.sensor(poke_interval=30, timeout=300, mode='poke')
     def is_api_available() -> PokeReturnValue:
-        api = BaseHook.get_connection('meta_stock_api')
+        api = BaseHook.get_connection('stock_api')
         url = f"{api.host}{api.extra_dejson['endpoint']}"
         response = requests.get(url, headers=api.extra_dejson['headers'])
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition, xcom_value=url)
 
-    is_api_available()
+    get_stock_prices = PythonOperator(
+        task_id='get_stock_price',
+        python_callable=_get_stock_prices,
+        op_kwargs={
+            'url': '{{ task_instance.xcom_pull(task_ids="is_api_available") }}',
+            'symbol': SYMBOL
+        }
+    )
+
+    is_api_available() >> get_stock_prices
 
 stock_market()
